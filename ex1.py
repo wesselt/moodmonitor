3e4c46463137a178051dcf43776b83bc302de81e
j.winter@operations <johan.winter@kpn.com>
2017-11-03 00:29:50 +0100

    remove unused files

--- ex1.py
+++ ex1.py
@@ -1,6 +1,6 @@
-print "Hello World!"
-print "Hello again"
-print "I like typing this."
-print 'Yay! Printing.'
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
+print("Hello World!")
+print("Hello again")
+print("I like typing this.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
