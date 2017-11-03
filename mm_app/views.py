568154ee5e5568cf1e7f3a806413df1a334c7ceb
j.winter@operations <johan.winter@kpn.com>
2017-11-03 08:20:32 +0100

    add html

---
+++ mm_app/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def home(request):
+    context = {'title': 'Welkom on our Mood Monitor'}
+    return render(request, 'home.html', context)
